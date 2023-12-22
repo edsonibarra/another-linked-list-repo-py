1cd34c60c56b7a7262fa9b81b0206aca9cb2ee51
edsonibarra <edsonibarra97@gmail.com>
2023-12-22 12:20:38 -0600

    TEST: validate print_list method

--- test_linked_list.py
+++ test_linked_list.py
@@ -10,3 +10,11 @@ def populated_linked_list() -> LinkedList:
     for n in range(1, 11):
         ll.append(n)
     return ll
+
+def test_print_linked_list(populated_linked_list):
+    ll = populated_linked_list
+    ll.print_list()
+    current_node = ll.head
+    for n in range(1, 11):
+        assert current_node.data == n
+        current_node = current_node.next
